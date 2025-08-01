bcf4cd9bc58069d3d8c855a86bdb8aaf0d3d34ae
Raven Scott <snxraven@gmail.com>
2025-08-01 06:36:31 +0000

    Move to holesail-logger 1.1.0

--- index.js
+++ index.js
@@ -2,22 +2,11 @@ const dgram = require('bare-dgram')
 const EventEmitter = require('events')
 
 function connPiper (connection, _dst, opts = {}, stats = {}) {
-  const debug = (msg) => {
-    if (opts.logger) opts.logger.debug(msg)
-  }
-  const info = (msg) => {
-    if (opts.logger) opts.logger.log(msg)
-  }
-  const warn = (msg) => {
-    if (opts.logger) opts.logger.warn(msg)
-  }
-  const error = (msg) => {
-    if (opts.logger) opts.logger.error(msg)
-  }
-  info('Starting TCP connPiper')
+  const logger = opts.logger || { log: () => {} }
+  logger.log({ type: 1, msg: 'Starting TCP connPiper' })
   const loc = _dst()
   if (loc === null) {
-    warn('Connection rejected (null destination)')
+    logger.log({ type: 2, msg: 'Connection rejected (null destination)' })
     connection.destroy() // don't return rejection error
     if (!stats.rejectCnt) {
       stats.rejectCnt = 0
@@ -35,35 +24,35 @@ function connPiper (connection, _dst, opts = {}, stats = {}) {
   stats.remCnt++
   let destroyed = false
   loc.on('data', d => {
-    debug(`Data from local to remote: ${d.length} bytes`)
+    logger.log({ type: 0, msg: `Data from local to remote: ${d.length} bytes` })
     connection.write(d)
   })
   connection.on('data', d => {
-    debug(`Data from remote to local: ${d.length} bytes`)
+    logger.log({ type: 0, msg: `Data from remote to local: ${d.length} bytes` })
     loc.write(d)
   })
   loc.on('error', destroy).on('close', destroy)
   connection.on('error', destroy).on('close', destroy)
   loc.on('end', () => {
-    debug('Local end, ending connection')
+    logger.log({ type: 0, msg: 'Local end, ending connection' })
     connection.end()
   })
   connection.on('end', () => {
-    debug('Connection end, ending local')
+    logger.log({ type: 0, msg: 'Connection end, ending local' })
     loc.end()
   })
   loc.on('connect', err => {
     if (err) {
-      error(err.message)
+      logger.log({ type: 3, msg: err.message })
     } else {
-      info('Connected')
+      logger.log({ type: 1, msg: 'Connected' })
     }
   })
   function destroy (err) {
     if (destroyed) {
       return
     }
-    info(`Destroying connPiper${err ? ` due to error: ${err.message}` : ''}`)
+    logger.log({ type: 1, msg: `Destroying connPiper${err ? ` due to error: ${err.message}` : ''}` })
     stats.locCnt--
     stats.remCnt--
     destroyed = true
@@ -81,24 +70,13 @@ function connPiper (connection, _dst, opts = {}, stats = {}) {
 class UdpSocket {
   constructor (opts) {
     this.opts = opts
+    this.logger = this.opts.logger || { log: () => {} }
     this.server = dgram.createSocket('udp4')
     this.client = dgram.createSocket('udp4')
     this.event = new EventEmitter()
     this.rinfo = null
-    this.debug = (msg) => {
-      if (this.opts.logger) this.opts.logger.debug(msg)
-    }
-    this.info = (msg) => {
-      if (this.opts.logger) this.opts.logger.log(msg)
-    }
-    this.warn = (msg) => {
-      if (this.opts.logger) this.opts.logger.warn(msg)
-    }
-    this.error = (msg) => {
-      if (this.opts.logger) this.opts.logger.error(msg)
-    }
     this.connect()
-    this.info(`UDP socket created${opts.bind ? `, binding to ${opts.host}:${opts.port}` : ''}`)
+    this.logger.log({ type: 1, msg: `UDP socket created${opts.bind ? `, binding to ${opts.host}:${opts.port}` : ''}` })
   }
 
   connect () {
@@ -106,22 +84,22 @@ class UdpSocket {
       this.server.bind(this.opts.port, this.opts.host)
     }
     this.server.on('message', (msg, rinfo) => {
-      this.debug(`UDP message from server: ${msg.length} bytes from ${rinfo.address}:${rinfo.port}`)
+      this.logger.log({ type: 0, msg: `UDP message from server: ${msg.length} bytes from ${rinfo.address}:${rinfo.port}` })
       this.event.emit('message', msg, rinfo)
       this.rinfo = rinfo
     })
     this.client.on('message', (response, rinfo) => {
-      this.debug(`UDP message from client: ${response.length} bytes from ${rinfo.address}:${rinfo.port}`)
+      this.logger.log({ type: 0, msg: `UDP message from client: ${response.length} bytes from ${rinfo.address}:${rinfo.port}` })
       this.event.emit('message', response)
     })
     this.client.on('error', (err) => {
-      this.error(`UDP error: ${err.stack}`)
+      this.logger.log({ type: 3, msg: `UDP error: ${err.stack}` })
       this.client.close()
     })
   }
 
   write (msg) {
-    this.debug(`Writing UDP message: ${msg.length} bytes`)
+    this.logger.log({ type: 0, msg: `Writing UDP message: ${msg.length} bytes` })
     if (this.rinfo) {
       this.server.send(msg, 0, msg.length, this.rinfo.port, this.rinfo.address)
     } else {
@@ -133,27 +111,15 @@ class UdpSocket {
 class UdpConnPiper {
   constructor (remote, local, opts = {}) {
     this.opts = opts
+    this.logger = this.opts.logger || { log: () => {} }
     this.remote = remote
     this.local = local
     this.client = opts.client
-    this.debugOpt = opts.debug || false
     this.retryDelay = opts.retryDelay || 2000
     this.destroyed = false
-    this.debug = (msg) => {
-      if (this.opts.logger) this.opts.logger.debug(msg)
-    }
-    this.info = (msg) => {
-      if (this.opts.logger) this.opts.logger.log(msg)
-    }
-    this.warn = (msg) => {
-      if (this.opts.logger) this.opts.logger.warn(msg)
-    }
-    this.error = (msg) => {
-      if (this.opts.logger) this.opts.logger.error(msg)
-    }
     this._bindListeners()
     this.connect()
-    this.info(`Starting UDP piper${opts.client ? ' (client mode)' : ' (server mode)'}`)
+    this.logger.log({ type: 1, msg: `Starting UDP piper${opts.client ? ' (client mode)' : ' (server mode)'}` })
   }
 
   _bindListeners () {
@@ -169,12 +135,12 @@ class UdpConnPiper {
 
   connect () {
     if (this.destroyed) return
-    this.debug('Connecting UDP piper')
+    this.logger.log({ type: 0, msg: 'Connecting UDP piper' })
     this.removeListeners()
     this.localStream = typeof this.local === 'function' ? this.local() : this.local
     this.remoteStream = typeof this.remote === 'function' ? this.remote() : this.remote
     if (!this.localStream || !this.remoteStream) {
-      this.warn('UDP connect failed (missing streams)')
+      this.logger.log({ type: 2, msg: 'UDP connect failed (missing streams)' })
       this.destroy()
       return
     }
@@ -188,7 +154,7 @@ class UdpConnPiper {
     this.remoteStream.on('message', this.bound.onConnectionMessage)
     this.remoteStream.on('error', this.bound.onConnectionError)
     this.remoteStream.on('close', this.bound.onConnectionClose)
-    this.debug('UDP listeners attached')
+    this.logger.log({ type: 0, msg: 'UDP listeners attached' })
   }
 
   removeListeners () {
@@ -202,37 +168,37 @@ class UdpConnPiper {
       this.remoteStream.off('error', this.bound.onConnectionError)
       this.remoteStream.off('close', this.bound.onConnectionClose)
     }
-    this.debug('UDP listeners removed')
+    this.logger.log({ type: 0, msg: 'UDP listeners removed' })
   }
 
   onLocMessage (msg, rinfo) {
-    this.debug(`UDP message from local: ${msg.length} bytes`)
+    this.logger.log({ type: 0, msg: `UDP message from local: ${msg.length} bytes` })
     if (this.remoteStream && !this.destroyed) {
       this.remoteStream.trySend?.(msg)
     }
   }
 
   onConnectionMessage (msg) {
-    this.debug(`UDP message from connection: ${msg.length} bytes`)
+    this.logger.log({ type: 0, msg: `UDP message from connection: ${msg.length} bytes` })
     if (this.localStream && !this.destroyed) {
       this.localStream.write?.(msg)
     }
   }
 
   _handleError (err) {
-    this.warn(`UDP error: ${err ? err.message : 'close'}`)
+    this.logger.log({ type: 2, msg: `UDP error: ${err ? err.message : 'close'}` })
     this.destroy(err)
   }
 
   destroy (err) {
     if (this.destroyed) return
     this.destroyed = true
-    this.info(`Destroying UDP piper${err ? ` due to error: ${err.message}` : ''}`)
+    this.logger.log({ type: 1, msg: `Destroying UDP piper${err ? ` due to error: ${err.message}` : ''}` })
     this.removeListeners()
     try { this.localStream?.destroy?.(err) } catch (e) {}
     try { this.remoteStream?.close?.(err) } catch (e) {}
     if (this.client) {
-      this.info(`Scheduling retry in ${this.retryDelay}ms`)
+      this.logger.log({ type: 1, msg: `Scheduling retry in ${this.retryDelay}ms` })
       setTimeout(() => {
         this.destroyed = false
         this.connect()
